c73c641179a3e193a2a07d96bd01dfe2fafd882f
Pola03 <paula.silvalara030410@gmail.com>
2023-10-18 00:21:57 -0400

    MLFQ

--- src/scheduler.c
+++ src/scheduler.c
@@ -6,13 +6,20 @@
 #include <time.h>
 
 #include "simulation.h"
+typedef struct node
+{
+  proc_info_t proc;
+  struct node *sig;
+  struct node *ant;
+  int isNull;
+} node;
 
 int last_proc_pos = 0;
-//node *roots[3] = {NULL, NULL, NULL};
-//node *fondos[3] = {NULL, NULL, NULL};
-int queue_id;
-proc_info_t* process;
-int S =  100; //Tiempo del priority boost
+node *roots[3] = {NULL, NULL, NULL};
+node *fondos[3] = {NULL, NULL, NULL};
+int queue_id = -1;
+node process;
+int S = 1000; // Tiempo del priority boost
 
 // La función que define un scheduler está compuesta por los siguientes
 // parámetros:
@@ -128,40 +135,36 @@ int Round_Robin(proc_info_t *procs_info, int procs_count, int curr_time,
   return procs_info[last_proc_pos].pid;
 }
 
-/*typedef struct node
-{
-  proc_info_t *proc;
-  struct node *sig;
-  struct node *ant
-} node;
-
-void Push(proc_info_t *x, int queue_id)
+void Push(proc_info_t x, int queue) // OJO
 {
 
-  node *root = roots[queue_id - 1];
-  node *fondo = fondos[queue_id - 1];
+  node **root_pointer = &roots[queue - 1];
+  node **fondo_pointer = &fondos[queue - 1];
 
   node *nuevo;
-  nuevo = malloc(sizeof(struct node));
+  nuevo = (node *)malloc(sizeof(struct node));
   nuevo->proc = x;
   nuevo->sig = NULL;
-  nuevo->ant = fondo;
-  if (root == NULL)
+  nuevo->ant = *fondo_pointer;
+  nuevo->isNull = 0;
+  // Si mi cola esta vacía, al entrar un elemento, tanto la raíz como el fondo estarán apuntando al nuevo elemento
+  if (*root_pointer == NULL)
   {
-    root = nuevo;
-    fondo = nuevo;
+    *root_pointer = nuevo;
+    *fondo_pointer = nuevo;
   }
+  // Si mi cola no está vacía, al entrar un elemento, el fondo estará apuntando al nuevo elemento
   else
   {
-    fondo->sig = nuevo;
-    fondo = nuevo;
+    (*fondo_pointer)->sig = nuevo;
+    *fondo_pointer = nuevo;
   }
 }
 
-node *GetNIO(node *root, node *fondo) // Obtener el primer proceso que no esta en IO
+node *GetNIO(node *root) // Esta función obtiene el proceso con mayor prioridad que no está en IO
 {
   node *x = root;
-  while ((*x->proc).on_io && x != NULL)
+  while (x != NULL && (x->proc).on_io)
   {
     x = x->sig;
   }
@@ -169,57 +172,60 @@ node *GetNIO(node *root, node *fondo) // Obtener el primer proceso que no esta e
   return x;
 }
 
-proc_info_t* Pop(node *fondo, node *root, node *x)
+node Pop(node **root_pointer, node **fondo_pointer, node *x)
 {
-  // Si todos los procesos están en IO
+  node y;
+  // Si todos los procesos están en IO, no se va a ejecutar ningún proceso
   if (x == NULL)
-    return NULL;//Si todos estan en IO retornar la raiz, proc_info_t
+  {
+    y.isNull = 1;
+    return y;
+  }
 
   // Si el primer proceso que no esta en IO es la root
 
-  if (x == root)
+  if (x == *root_pointer)
   {
-    if (root == fondo) // La cola solo tenia un elemento
+    if (*root_pointer == *fondo_pointer) // La cola solo tenia un elemento
     {
-      root = NULL;
-      fondo = NULL;
+      *root_pointer = NULL;
+      *fondo_pointer = NULL;
     }
     else // La cola tenia mas de un elemento
     {
-      x->sig->ant = NULL;
-      root = root->sig;
+      (x->sig)->ant = NULL;
+      *root_pointer = (*root_pointer)->sig;
     }
   }
 
-  // Si el primer proceso que no esta en IO esta en IO es el fondo
-  else if (x == fondo)
+  // Si el primer proceso que no esta en IO es el fondo
+  else if (x == *fondo_pointer)
   {
-    fondo->ant->sig = NULL;
-    fondo = fondo->ant;
+    ((*fondo_pointer)->ant)->sig = NULL;
+    *fondo_pointer = (*fondo_pointer)->ant;
   }
-  // Si el primer proceso que no esta en el medio
+  // Si el primer proceso que no esta en IO esta en el medio
   else
   {
-    x->sig->ant = x->ant;
-    x->ant->sig = x->sig;
+    (x->sig)->ant = x->ant;
+    (x->ant)->sig = x->sig;
   }
-  proc_info_t *y = x->proc;
+  y = *x;
   free(x);
   return y;
 }
 
-proc_info_t* GetProcess() // Obtengo el proceso en orden de prioridad
+void GetProcess() // Obtengo el proceso al cual le corresponde ejecutarse, recorriendo las colas de prioridad
 {
   for (int i = 0; i < 3; i++)
   {
-    process = Pop(roots[i], fondos[i], GetNIO(roots[i], fondos[i]));
-    if (process != NULL)
+    process = Pop(&roots[i], &fondos[i], GetNIO(roots[i]));
+    if (!(process.isNull))
     {
       queue_id = i + 1;
-      return process;
+      return;
     }
   }
-  return NULL;
 }
 
 void SetQueue(int curr_pid) // Bajo la prioridad del proceso, si estaba en la cola 3 lo inserto al final de la cola
@@ -228,33 +234,111 @@ void SetQueue(int curr_pid) // Bajo la prioridad del proceso, si estaba en la co
   {
     if (queue_id == 1 || queue_id == 2)
     {
-      Push(process, queue_id + 1);
+      Push(process.proc, queue_id + 1);
     }
     else
     {
-      Push(process, 3);
+      Push(process.proc, 3);
     }
   }
 }
 
-void SetMaxQueue(int curr_time)
+void SetMaxQueue(int curr_time) // Cada cierto tiempo S, todos los procesos se ubican en la cola de mayor prioridad
 {
-  if (curr_time == 100)
+  if (curr_time % 1000 == 0)
   {
     int queue = 3;
     while (queue > 1)
     {
-      while (roots[queue-1]!=NULL)
+      while (roots[queue - 1] != NULL)
       {
-         proc_info_t* max_priority = Pop(roots[queue-1],fondos[queue-1],roots[queue-1]);
-        Push(max_priority,queue);
+        node max_priority = Pop(&roots[queue - 1], &fondos[queue - 1], roots[queue - 1]);
+        Push(max_priority.proc, 1);
       }
 
       queue--;
-      
     }
   }
-}*/
+}
+
+int FindProcess(proc_info_t proc)
+{
+  node *pointer;
+  for (int i = 0; i < 3; i++)
+  {
+    pointer = roots[i];
+    while (pointer != NULL)
+    {
+      if (proc.pid == (pointer->proc).pid)
+      {
+        return 1;
+      }
+      pointer = pointer->sig;
+    }
+  }
+  return 0;
+}
+
+int MLFQ(proc_info_t *procs_info, int procs_count, int curr_time,
+         int curr_pid)
+{
+  // if (curr_time == 890 || curr_time == 900)
+  // {
+  //   printf("Antes de hacer todo\n");
+  //   node *pointer;
+  //   for (int i = 0; i < 3; i++)
+  //   {
+  //     pointer = roots[i];
+  //     while (pointer != NULL)
+  //     {
+  //       printf("Cola: %d ", i + 1);
+  //       printf("Elem_pid: %d\n", (pointer->proc).pid);
+  //       pointer = pointer->sig;
+  //     }
+
+  //     printf("-----------------\n");
+  //   }
+  // }
+  // printf("El curr_pid es: %d\n", curr_pid);
+  SetQueue(curr_pid); // Si el proceso que se estaba ejecutando anteriormente no ha finalizado, le bajo la prioridad
+
+  // Si entran procesos nuevos, los añado a la cola de mayor prioridad
+  for (int i = 0; i < procs_count; i++)
+  {
+    if (!FindProcess(procs_info[i]))
+    {
+      Push(procs_info[i], 1);
+    }
+  }
+
+  SetMaxQueue(curr_time); // Cada cierto tiempo S, todos los procesos se ubican en la cola de mayor prioridad
+  GetProcess();           // Obtengo un puntero que me indica el proceso con mayor prioridad
+
+  // if (curr_time == 890 || curr_time == 900)
+  // {
+  //   printf("Despues de hacer todo\n");
+  //   node *pointer;
+  //   for (int i = 0; i < 3; i++)
+  //   {
+  //     pointer = roots[i];
+  //     while (pointer != NULL)
+  //     {
+  //       printf("Cola: %d ", i + 1);
+  //       printf("Elem_pid: %d\n", (pointer->proc).pid);
+  //       pointer = pointer->sig;
+  //     }
+
+  //     printf("-----------------\n");
+  //   }
+  // }
+
+  if (process.isNull)
+    return -1;
+  // printf("Proceso a ejecutar: %d\n", (process.proc).pid);
+  // printf("La cola es: %d\n", queue_id);
+
+  return (process.proc).pid;
+}
 
 // Esta función devuelve la función que se ejecutará en cada timer-interrupt
 // según el nombre del scheduler.
@@ -271,6 +355,8 @@ schedule_action_t get_scheduler(const char *name)
     return *STCF;
   if (strcmp(name, "rr") == 0)
     return *Round_Robin;
+  if (strcmp(name, "mlfq") == 0)
+    return *MLFQ;
 
   fprintf(stderr, "Invalid scheduler name: '%s'\n", name);
   exit(1);
